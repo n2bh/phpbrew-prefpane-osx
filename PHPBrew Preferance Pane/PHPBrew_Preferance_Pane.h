52005e477e8205ee77ea11ec31c11ece8dc4c8cf
Thomas Roth <t.roth@ibrams.com>
2013-06-21 07:58:16 +0200

    Initial Commit

---
+++ PHPBrew Preferance Pane/PHPBrew_Preferance_Pane.h
@@ -0,0 +1,15 @@
+//
+//  PHPBrew_Preferance_Pane.h
+//  PHPBrew Preferance Pane
+//
+//  Created by Thomas Roth on 21.06.13.
+//  Copyright (c) 2013 Codecrafters. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface PHPBrew_Preferance_Pane : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ PHPBrew Preferance Pane/PHPBrew_Preferance_Pane.m
@@ -0,0 +1,17 @@
+//
+//  PHPBrew_Preferance_Pane.m
+//  PHPBrew Preferance Pane
+//
+//  Created by Thomas Roth on 21.06.13.
+//  Copyright (c) 2013 Codecrafters. All rights reserved.
+//
+
+#import "PHPBrew_Preferance_Pane.h"
+
+@implementation PHPBrew_Preferance_Pane
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
